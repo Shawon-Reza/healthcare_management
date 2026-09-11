9b9d82a533db124fb90880e55dfa01509f01c70b
Shawon Reza <shawon.reza.sr81@gmail.com>
2026-09-11 17:45:00 +0600

    feat(seed): Admin seeding vai prisma seed.ts file under prisma folder

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+
+import { prisma } from "../src/lib/prisma";
+import { auth } from "../src/lib/auth";
+import { Role } from "../src/generated/prisma/enums";
+
+async function main() {
+    
+    const email = "superadmin@egmail.com";
+
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    console.log("Existing user:", existingUser);
+
+    if (existingUser) {
+        console.log("Super Admin already exists.");
+        return;
+    }
+
+    const superAdmin = await auth.api.signUpEmail({
+        body: {
+            name: "superAdmin",
+            email,
+            password: "superAdmin@egmail.com",
+            role: Role.SUPER_ADMIN,
+        },
+    });
+
+    console.log("Super Admin created:", superAdmin);
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
--- src/routes/indexRoute.ts
+++ src/routes/indexRoute.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 import { specialityRouter } from "../modules/specialty/specialty.route";
 import { authRouter } from "../modules/auth/auth.router";
 import { doctorRouter } from "../modules/doctor/doctor.router";
+import { adminRouter } from "../modules/admin/admin.route";
+import { superAdminRouter } from "../modules/superAdmin/superAdmin.router";
 
 
 export const indexRouter = Router();
 
-
-indexRouter.use("/specialties",specialityRouter);
-indexRouter.use("/auth",authRouter);
+indexRouter.use("/super_admin", superAdminRouter);
+indexRouter.use("/admin", adminRouter);
 indexRouter.use("/doctors", doctorRouter);
+
+indexRouter.use("/auth", authRouter);
+
+indexRouter.use("/specialties", specialityRouter);
+
+
